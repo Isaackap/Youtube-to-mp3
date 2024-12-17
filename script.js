daec79c105cf4deff3af0ca97f7313daa436a631
Isaac Kapeel <ikapeel@gmail.com>
2024-12-16 23:03:57 -0600

    more changes

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 console.log("Adding event listener to 'convert' button");
-document.getElementById('convert').addEventListener('click', async(event) =>{
+
+var onClick = async(event) =>{
+    console.log(event);
     console.log("Button clicked");
     event.preventDefault();
-    const youtubeURL = document.getElementById('url').value;
-    const urlCheck = youtubeURL.match(/:\/\/www.youtube.com\/.*?\bv=([^&]+)/)
+    event.stopImmediatePropagation();
+    var youtubeURL = document.getElementById('url').value;
+    var urlCheck = youtubeURL.match(/:\/\/www.youtube.com\/.*?\bv=([^&]+)/)
 
     if(!youtubeURL){
 
@@ -16,24 +19,24 @@ document.getElementById('convert').addEventListener('click', async(event) =>{
 
         try{
             console.log("Sending POST request....");
-            const response = await fetch('http://localhost:3000/convert', {
+            var response = await fetch('http://localhost:3000/convert', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'}, 
                 body: JSON.stringify({youtubeURL})
             });
             console.log("POST request completed.");
             console.log("Response status: ", response.status);
-    
+            
             if(response.ok){
-                const data = await response.json();
+                var data = await response.json();
                 console.log("success", data);
                 //localStorage.setItem('savedFile', data.fileName);
                 //let file = localStorage.getItem('savedFile');
                 //console.log(file);
-                let elements = document.getElementsByClassName('form-convert');
+                /*let elements = document.getElementsByClassName('form-convert');
                 for(let i = 0; i < elements.length; i++){
                     elements[i].style.display = 'none';
-                }
+                }*/
             } else{
                 console.error("Error response: ", await response.json());
             }
@@ -42,7 +45,10 @@ document.getElementById('convert').addEventListener('click', async(event) =>{
             console.error("Request failed: ", error);
         }
     }
-});
+}
+
+document.getElementById('convert').removeEventListener('click', onClick);
+document.getElementById('convert').addEventListener('click', onClick);
 //document.querySelectorAll('form-convert').style.display = 'none';
 
 //document.getElementById('form-convert').style.display = "none";
